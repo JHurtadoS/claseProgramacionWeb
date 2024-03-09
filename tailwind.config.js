c90d82939082a0163356fb427b28fb21292bc89b
juanH <86845748+JHurtadoS@users.noreply.github.com>
2024-03-09 16:30:25 +0000

    Fisrst setting astro proyect with colors and fonts to parcial one

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+      marcellus: ["Marcellus", "sans-serif"],
+    },
     extend: {
-      fontFamily: {
-        'blw': ['"Barlow"'],
+      colors: {
+        cyan: "#50d4ec",
+        black: "#383434",
+        grey: "#b2b3b3",
+        neutralBlue: "#337ab7",
+        rose: "#9c2759",
+        greenNeutal: "#157a8a",
       },
     },
   },
   plugins: [],
-}
+};
