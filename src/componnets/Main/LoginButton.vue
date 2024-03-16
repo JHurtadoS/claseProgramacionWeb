d8acced54026ffe9b298e9fa18b7a52ec755bc60
Juan Esteban Hurtado <jhurtados@car.gov.co>
2024-03-16 01:44:42 -0500

    agregando vuejs a astro y probando con un nuevo componente para rempalzar el antigua boton y hacerlo reactivo segun el estado ademas agregar las nano store. nueva seccion descripcion

---
+++ src/componnets/Main/LoginButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <a class=" text-white text-lg flex gap-2 bg-neutralBlue p-3  rounded-sm" href="#"> 
+                <User className="h-6 w-6"/> Iniciar Sesion
+    </a>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
