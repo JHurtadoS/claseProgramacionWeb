fd8f81807cc547a7d8a328d80eead68f0365f228
juanH <86845748+JHurtadoS@users.noreply.github.com>
2024-03-21 02:42:40 +0000

    footer pt 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,10 @@ export default {
         black: "#383434",
         grey: "#b2b3b3",
         neutralBlue: "#337ab7",
-        neutralBlue2:"#306ca4",
+        neutralBlue2: "#28679e",
         rose: "#9c2759",
         greenNeutal: "#157a8a",
-        darkBlue:"#103c64"
-
+        darkBlue: "#103c64",
       },
     },
   },
