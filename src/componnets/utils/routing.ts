b1c5285061387f672d1503202c74a51104f17b88
Juan Esteban Hurtado <jhurtados@car.gov.co>
2024-05-11 10:36:45 -0500

    commit unico

--- src/componnets/utils/routing.ts
+++ src/componnets/utils/routing.ts
@@ -19,15 +19,10 @@ export const preventReloadAnchor=(links:NodeListOf<HTMLAnchorElement>)=>{
 
 export const ItsOnSameLink=(links:NodeListOf<HTMLLIElement>)=>{
     const currentUrl = obtenerRutaDespuesDeAdmin(window.location.href);
-
     links.forEach((e) => {
-
         const anchor = e.querySelector("a")
         if(anchor && currentUrl === anchor.pathname ){
-            console.log(e)
             e.classList.add("bg-neutralBlue")
-
         }
-
     });
 }
